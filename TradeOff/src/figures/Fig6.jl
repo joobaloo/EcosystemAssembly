12c969f009cc44896aa8e07c83e156cb32508759
Jacob Cook <jc2017@ic.ac.uk>
2024-07-08 15:40:55 +0100

    Switched to OS independant file paths for figure plotting scripts

--- TradeOff/src/figures/Fig6.jl
+++ TradeOff/src/figures/Fig6.jl
@@ -27,44 +27,46 @@ function figure6(ims::Int64)
     b = [bt[10]; bt[9]; bt[7]; bt[5]]
     # Make plot objects
     p1 = plot(xlabel = "Times (s)",
-              xlim = (-Inf, 5e7),
-              legend = :right,
-              ylabel = L"\eta",
-              title = "ATP yield",
-              ylim = (1.0, 6.0))
+        xlim = (-Inf, 5e7),
+        legend = :right,
+        ylabel = L"\eta",
+        title = "ATP yield",
+        ylim = (1.0, 6.0))
     p2 = plot(xlabel = "Times (s)",
-              xlim = (-Inf, 5e7),
-              ylim = (0.55, 0.75),
-              legend = false,
-              ylabel = "Fraction of free-energy transduced",
-              title = "Average reaction efficiency")
+        xlim = (-Inf, 5e7),
+        ylim = (0.55, 0.75),
+        legend = false,
+        ylabel = "Fraction of free-energy transduced",
+        title = "Average reaction efficiency")
     p3 = plot(xlabel = "Times (s)",
-              xlim = (-Inf, 5e7),
-              ylim = (2.25, 3.00),
-              legend = false,
-              ylabel = "Average number of reaction steps",
-              title = "Relative frequency of reaction types")
+        xlim = (-Inf, 5e7),
+        ylim = (2.25, 3.00),
+        legend = false,
+        ylabel = "Average number of reaction steps",
+        title = "Relative frequency of reaction types")
     # Loop over the 4 conditions
     for i in 1:2
         # Extract other simulation parameters from the function
         Np, Nt, M, d, μrange = sim_paras(i)
+        data_dir = joinpath(
+            pwd(), "Output", "$(Np)Pools$(M)Metabolites$(Nt)Speciesd=$(d)u=$(μrange)")
         # Find file name to load in
-        tfile = "Output/$(Np)Pools$(M)Metabolites$(Nt)Speciesd=$(d)u=$(μrange)/RunStats$(ims)Ims.jld"
+        stats_file = joinpath(data_dir, "RunStats$(ims)Ims.jld")
         # Check it actually exists
-        if ~isfile(tfile)
+        if ~isfile(stats_file)
             error("missing stats file for $(ims) immigrations simulations")
         end
         # Now load out the times, and number of trajectories
-        times = load(tfile, "times")
-        no_via = load(tfile, "no_via")
+        times = load(stats_file, "times")
+        no_via = load(stats_file, "no_via")
         # Load in averages
-        mn_via_η_bw = load(tfile, "mn_via_η_bw")
-        mn_fr_ΔG_bw = load(tfile, "mn_fr_ΔG_bw")
-        mn_av_steps_bw = load(tfile, "mn_av_steps_bw")
+        mn_via_η_bw = load(stats_file, "mn_via_η_bw")
+        mn_fr_ΔG_bw = load(stats_file, "mn_fr_ΔG_bw")
+        mn_av_steps_bw = load(stats_file, "mn_av_steps_bw")
         # Load in standard deviations
-        sd_via_η_bw = load(tfile, "sd_via_η_bw")
-        sd_fr_ΔG_bw = load(tfile, "sd_fr_ΔG_bw")
-        sd_av_steps_bw = load(tfile, "sd_av_steps_bw")
+        sd_via_η_bw = load(stats_file, "sd_via_η_bw")
+        sd_fr_ΔG_bw = load(stats_file, "sd_fr_ΔG_bw")
+        sd_av_steps_bw = load(stats_file, "sd_av_steps_bw")
         # Calculate relevant standard errors
         se_via_η_bw = sd_via_η_bw ./ sqrt.(no_via)
         se_fr_ΔG_bw = sd_fr_ΔG_bw ./ sqrt.(no_via)
@@ -78,7 +80,9 @@ function figure6(ims::Int64)
     end
     # Load in no immigration data
     Np, Nt, M, d, μrange = sim_paras(5)
-    noim_file = "Output/NoImm$(Np)Pools$(M)Metabolites$(Nt)Speciesd=$(d)u=$(μrange)/RunStats0Ims.jld"
+    data_dir = joinpath(
+        pwd(), "Output", "NoImm$(Np)Pools$(M)Metabolites$(Nt)Speciesd=$(d)u=$(μrange)")
+    noim_file = joinpath(data_dir, "RunStats0Ims.jld")
     # Check it actually exists
     if ~isfile(noim_file)
         error("missing stats file for no immigrations simulations")
@@ -92,23 +96,22 @@ function figure6(ims::Int64)
     se_via_η_bw = sd_via_η_bw ./ sqrt.(no_via)
     # Then plot
     p4 = plot(times,
-              mn_via_η_bw,
-              ribbon = se_via_η_bw,
-              color = a[3],
-              label = "high free-energy",
-              xlim = (-Inf, 2.5e6),
-              ylim = (1.0, 6.0),
-              title = "ATP yield without immigration")
+        mn_via_η_bw,
+        ribbon = se_via_η_bw,
+        color = a[3],
+        label = "high free-energy",
+        xlim = (-Inf, 2.5e6),
+        ylim = (1.0, 6.0),
+        title = "ATP yield without immigration")
     plot!(p4, xlabel = "Times (s)", ylabel = L"\eta")
-    # Check if directory exists and if not make it
-    if ~isdir("Output/Fig6")
-        mkdir("Output/Fig6")
-    end
+    # Define output directory and if necessary make it
+    outdir = joinpath(pwd(), "Output", "Fig6")
+    mkpath(outdir)
     # Save figures to this directory
-    savefig(p1, "Output/Fig6/Eta.png")
-    savefig(p2, "Output/Fig6/Efficiency.png")
-    savefig(p3, "Output/Fig6/Steps.png")
-    savefig(p4, "Output/Fig6/NoImmEta.png")
+    savefig(p1, joinpath(outdir, "Eta.png"))
+    savefig(p2, joinpath(outdir, "Efficiency.png"))
+    savefig(p3, joinpath(outdir, "Steps.png"))
+    savefig(p4, joinpath(outdir, "NoImmEta.png"))
     # Add annotations
     px, py = annpos([0.0; 5e7], [1.0; 6.0], 0.075, 0.05)
     annotate!(p1, px, py, text("A", 17, :black))
@@ -120,7 +123,7 @@ function figure6(ims::Int64)
     annotate!(p4, px, py, text("D", 17, :black))
     # Plot all graphs as a single figure
     pt = plot(p1, p2, p3, p4, layout = (2, 2), size = (1200, 800), margin = 5.0mm)
-    savefig(pt, "Output/Fig6/figure6.png")
+    savefig(pt, joinpath(outdir, "figure6.png"))
     return (nothing)
 end
 
